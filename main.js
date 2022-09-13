70882e8e265a74bd1670b4d5fc817ccc64ca57d0
Sanjana <sanjucheerla@gmail.com>
2022-09-13 19:46:05 -0400

    worked with mdpatel2, edited intents

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
-const Discord = require("discord.js");
-
-const client = new Discord.Client()
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`)
