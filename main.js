58f0673d7ba6bf381e3c72bde60e8191fc4c57d7
Sanjana <sanjucheerla@gmail.com>
2022-09-13 20:10:55 -0400

    worked with mdpatel2, have ping function working

--- main.js
+++ main.js
@@ -4,9 +4,11 @@ require('dotenv').config()
 const { Client, GatewayIntentBits } = require('discord.js');
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-
 const { REST, Routes } = require('discord.js');
+const clientId = '1017062749538361474';
+const guildId = '1011989055736660061';
 
+// slash commands
 const commands = [
   {
     name: 'ping',
@@ -15,13 +17,14 @@ const commands = [
 ];
 
 
+//register slash commands
 const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
 
 (async () => {
   try {
     console.log('Started refreshing application (/) commands.');
 
-    await rest.put(Routes.applicationCommands(1017062749538361474), { body: commands });
+    await rest.put(Routes.applicationCommands(clientId), { body: commands });
 
     console.log('Successfully reloaded application (/) commands.');
   } catch (error) {
