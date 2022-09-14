905bbca523b170b66b8bd3d1bfef25afdd9849d9
Sanjana <sanjucheerla@gmail.com>
2022-09-13 20:06:07 -0400

    worked with mdpatel2, have env working

--- main.js
+++ main.js
@@ -1,14 +1,46 @@
+//do not delete needed for env
+require('dotenv').config()
+
 const { Client, GatewayIntentBits } = require('discord.js');
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-client.on("ready", () => {
-  console.log(`Logged in as ${client.user.tag}!`)
-})
 
-client.on("message", msg => {
-  if (msg.content === "ping") {
-    msg.reply("pong");
+const { REST, Routes } = require('discord.js');
+
+const commands = [
+  {
+    name: 'ping',
+    description: 'Replies with Pong!',
+  },
+];
+
+
+const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
+
+(async () => {
+  try {
+    console.log('Started refreshing application (/) commands.');
+
+    await rest.put(Routes.applicationCommands(1017062749538361474), { body: commands });
+
+    console.log('Successfully reloaded application (/) commands.');
+  } catch (error) {
+    console.error(error);
   }
-})
+})();
+
+
+// run bot
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+  });
+  
+  client.on('interactionCreate', async interaction => {
+    if (!interaction.isChatInputCommand()) return;
+  
+    if (interaction.commandName === 'ping') {
+      await interaction.reply('Pong!');
+    }
+  })
 
 client.login(process.env.TOKEN)
