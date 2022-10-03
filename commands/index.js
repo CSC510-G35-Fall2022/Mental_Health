c3bc52b2a775ddbbff67a1f18bc075576e2f7f99
Sanjana <sanjucheerla@gmail.com>
2022-10-03 13:02:33 -0400

    fixed bug with module types

--- commands/index.js
+++
@@ -1,6 +0,0 @@
-import JournalCommand, { getData as getJournalData } from "./journal.js";
-import PingCommand, { getData as getPingData } from "./ping.js";
-
-export { JournalCommand, PingCommand };
-
-export default [getJournalData, getPingData];
