6239c3eca4088aefad79a5c5ef2d3a707ad0b310
SaailGanesh <saailganesh@gmail.com>
2022-10-04 21:01:27 -0400

    COMPLETED HELP COMMANDS

--- commands/help.js
+++ commands/help.js
@@ -1,10 +1,21 @@
+const fs = require('fs');
 const { SlashCommandBuilder } = require('@discordjs/builders');
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('help')
-        .setDescription('Replies with Holp'),
+        .setDescription('Provides Names and Description of commands'),
     async execute(interaction) {
-        interaction.reply({ content: 'Holp' })
-    }
+        let str
+        const commandFiles=fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+        for (const file of commandFiles) {
+            const command = require(`./${file}`);
+            str += `Name: ${command.data.name}, Description: ${command.data.description} \n`;
+            }
+    
+            return interaction.reply({
+            content: str,
+            ephemeral: true,
+            });
+    },
 };
