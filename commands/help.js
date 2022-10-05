b2043b7cd966fc614b02a7277b91a79358929c4a
SaailGanesh <saailganesh@gmail.com>
2022-10-05 16:14:17 -0400

    Embed command to help file

--- commands/help.js
+++ commands/help.js
@@ -1,21 +1,24 @@
 const fs = require('fs');
 const { SlashCommandBuilder } = require('@discordjs/builders');
-
+const { EmbedBuilder } = require("discord.js");
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('help')
         .setDescription('Provides Names and Description of commands'),
-    async execute(interaction) {
-        let str
-        const commandFiles=fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-        for (const file of commandFiles) {
-            const command = require(`./${file}`);
-            str += `Name: ${command.data.name}, Description: ${command.data.description} \n`;
+        async execute(interaction) {
+            let str;
+            str=``
+            const commandFiles=fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+            for (const file of commandFiles) {
+                const command = require(`./${file}`);
+                str += `Name: ${command.data.name},\n Description: ${command.data.description} \n\n`;
             }
-    
+            const embed = new EmbedBuilder()
+            .setColor(0x0099ff)
+            .setTitle("Names of Commands")
+            .setDescription(str);
             return interaction.reply({
-            content: str,
-            ephemeral: true,
+                embeds: [embed]
             });
     },
 };
